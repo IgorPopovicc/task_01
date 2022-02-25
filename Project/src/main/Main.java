526430a1fe165144e2721eb0b07b77abfdffa467
Igor Popovic <igorpopovic999@gmail.com>
2022-02-25 10:21:03 +0100

    import Main

--- Project/src/main/Main.java
+++ Project/src/main/Main.java
@@ -1,9 +1,15 @@
 package main;
 
+import domain.Car;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Car c = new Car.Builder("VolksWagen", "Golf 6" , 5).color("Blue").build();
+		System.out.println(c);
+		c.start();
+		System.out.println(c);
 
 	}
 
